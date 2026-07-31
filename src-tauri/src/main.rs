0d757249643bc7ca1244192a6faaf2ac66b44315
zipg <4047349+zipg@users.noreply.github.com>
2026-07-31 17:40:51 +0800

    ci: hide console for diagnostic windows exe

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,5 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// Prevents additional console window on Windows, DO NOT REMOVE!!
+#![cfg_attr(target_os = "windows", windows_subsystem = "windows")]
 
 fn main() {
     dbx_lib::run();
